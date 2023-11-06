52d21ef42c0386e5ef13bb70cd53f2297e3506e6
Aditya Yadav <adityayadav124501@gmail.com>
2023-11-06 13:36:08 +0530

    Assignment-12

--- Assignments/Assignment-12/main.js
+++ Assignments/Assignment-12/main.js
@@ -2,6 +2,13 @@ const taskList = document.getElementById("list");
 const input = document.getElementById("input");
 
 
+document.addEventListener("DOMContentLoaded", () => {
+    const storedItems = JSON.parse(localStorage.getItem("tasks"));
+    storedItems.forEach(element => {
+        getElementsWithContent(element);
+    });
+})
+
 const add = () => {
     const inputVal = input.value;
     if(input.value == ""){
@@ -10,6 +17,7 @@ const add = () => {
     }
     createDiv();
     input.value = "";
+    updateLocalStorage();
 }
 
 
@@ -43,10 +51,40 @@ const getElements = (newElement) => {
 
 }
 
+const getElementsWithContent = (content) => {
+
+    let newElement = document.createElement("div");
+    newElement.className = "item";
+    taskList.appendChild(newElement);
+    
+    let para = document.createElement("p");
+    para.className = "content";
+    para.textContent = content;
+    newElement.appendChild(para);
+
+    let icon1 = document.createElement("img");
+    icon1.src = "images/tick_159641.png"
+    icon1.className = "icons";
+    icon1.onclick = function () {done(this)};
+    newElement.appendChild(icon1);
+
+    let icon2 = document.createElement("img");
+    icon2.src = "images/trash_8357206.png"
+    icon2.className = "icons";
+    icon2.onclick = function () {del(this)};
+    newElement.appendChild(icon2);
+}
+
 const done = (target) => {
     target.parentNode.firstChild.classList.toggle("finished");
 }
 
 const del = (target) => {
     target.parentNode.remove();
+    updateLocalStorage();
+}
+
+const updateLocalStorage = () => {
+    const tasks = Array.from(taskList.children).map((task) => {return task.firstChild.textContent});
+    localStorage.setItem("tasks", JSON.stringify(tasks));
 }
