b182270a2f2c51fc99f0694c8eb8c230586d7836
Aditya Yadav <adityayadav124501@gmail.com>
2023-11-09 04:27:42 +0530

    Assignment-13

---
+++ Assignments/Assignment-13/main.js
@@ -0,0 +1,30 @@
+const loadingText = document.getElementById("loadingText");
+const loader = document.getElementById("loader");
+const internetStatus = document.getElementById("internetStatus");
+
+
+document.addEventListener("DOMContentLoaded",() => {
+    if(navigator.onLine){
+        internetStatus.textContent = "You are currently ONLINE!!!";
+        internetStatus.style.background = "green";
+    }else{
+        internetStatus.textContent = "You are currently OFFLINE!!!";
+        internetStatus.style.background = "red";
+    }
+    setTimeout(() => {
+        loader.style.display = "none";
+        loadingText.textContent = "Enjoy... :D"
+        window.open('form.html', 'Popup', 'height=850, width=700, screenX=500, screenY=500')
+    }, 10000);
+})
+
+window.addEventListener("online", () => {
+    internetStatus.textContent = "You Are Currently ONLINE!!!!";
+    internetStatus.style.background = "green";
+    console.log("online");
+})
+window.addEventListener("offline", () => {
+    internetStatus.textContent = "You Are Currently OFFLINE!!!!";
+    internetStatus.style.background = "red";
+    console.log("offline");
+})
